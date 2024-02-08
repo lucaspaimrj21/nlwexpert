81e9ccabb9dbde06b3cfd77e0809e8d70f1c0673
Lucas Paim <lucaspaimrj21@gmail.com>
2024-02-08 20:35:19 -0300

    feat: Implementing View for tag creator with Http Types

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,7 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
+
 
 class TagCreatorView:
     '''
@@ -7,13 +9,15 @@ class TagCreatorView:
     
     '''
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        # body = http_request.body
-        # product_code = body["product_code"]
+        tag_creator_controller = TagCreatorController()
+
+        body = http_request.body
+        product_code = body["product_code"]
 
         # logica de regra de negócio
-        print('Estou na minha view')
-        print(http_request)
+        formatted_response = tag_creator_controller.create(product_code)
+
 
         # retorno http
-        return HttpResponse(status_code=200, body={"hello": "world"})
+        return HttpResponse(status_code=200, body=formatted_response)
     
